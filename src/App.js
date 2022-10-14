532bb4be1b37e107c042863e4b72582ad3cf66f2
sanjayrtest <115560552+sanjayrtest@users.noreply.github.com>
2022-10-14 16:22:55 +0530

    Added canvas

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import BuildText from "./components/SideTab/Text/BuildText";
+import { useState } from "react";
 
 function App() {
+  const [text, setText] = useState("");
+  const [textColor, settextColor] = useState("#ffffff");
+  const [textFont, settextFont] = useState("Impact");
+  const [fontSize, setfontSize] = useState(20);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <BuildText
+          text={text}
+          setText={setText}
+          textColor={textColor}
+          setTextColor={settextColor}
+          textFont={textFont}
+          setTextFont={settextFont}
+          handleCollapse={() => {
+            console.log("handleCollapse");
+          }}
+          fontSize={fontSize}
+          setFontSize={setfontSize}
+          layerCloseHandler={() => {
+            console.log("handleCollapse");
+          }}
+        /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <canvas height={512} width={512} style={{ background: "white" }} />
         <a
           className="App-link"
           href="https://reactjs.org"
